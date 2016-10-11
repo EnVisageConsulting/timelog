3c80105225eaf9a9a7f513c9a43a8ad0290f844d
Micah <micahyb@gmail.com>
2016-10-10 21:56:00 -0500

    added getter and setter for project percent on logs
    renamed field to use percent getter and setter

--- app/models/project_log.rb
+++ app/models/project_log.rb
@@ -8,4 +8,17 @@ class ProjectLog < ApplicationRecord
   validates :description,         presence: true
   validates :total_allocation,    presence: true
   validates :billable_allocation, presence: true
+
+  # --- Setters & Getters --- #
+  def percent=value
+    value = value.chomp('%').to_i if value.is_a?(String)
+
+    self.total_allocation = value.nil? ? value : (value / 100.0).round(2)
+  end
+
+  def percent
+    return if total_allocation.nil?
+
+    (total_allocation * 100).round.to_s
+  end
 end
