9b450962cf453581bdacd4446420fedc6a961ab9
Micah <micahyb@gmail.com>
2016-10-13 00:56:44 -0500

    added deactivated projects view

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,13 @@
 module ProjectsHelper
+  def project_list
+    if params[:deactivated] == 'true'
+      Project.inactive.alphabetized
+    else
+      Project.active.alphabetized
+    end
+  end
+
+  def viewing_deactivated?
+    params[:deactivated] == 'true'
+  end
 end
