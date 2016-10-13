2dbf0be8365669ac231fabefb0d68e3b81b019a3
Micah <micahyb@gmail.com>
2016-10-13 00:39:47 -0500

    set up update and create actions
    whitelisted deactivation attribute for strong params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,17 +8,31 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_path, notice: "Succesfully created new project!" }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
   end
 
   def update
+    respond_to do |format|
+      if @project.update_attributes(project_params)
+        format.html { redirect_to projects_path, notice: "Successfully updated project!" }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   private
 
     def project_params
-      params.require(:project).permit(:name)
+      params.require(:project).permit(:name, :deactivated)
     end
 end
