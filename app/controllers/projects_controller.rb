d40812707b338df0d154cd9804b5cf414a0ec760
Micah <micahyb@gmail.com>
2016-10-12 23:02:04 -0500

    added project controller actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,24 @@
 class ProjectsController < ApplicationController
+  load_and_authorize_resource param_method: :project_params
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:name)
+    end
 end
