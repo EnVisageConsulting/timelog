a2e4b6425a8805fcc6578bf7d05f9ea8e3ce52a4
Micah <micahyb@gmail.com>
2016-10-13 00:38:58 -0500

    added getter and setter for project deactivation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
+require 'to_boolean'
+
 class Project < ApplicationRecord
   # --- Associations --- #
   has_many :client_projects
@@ -10,4 +12,14 @@ class Project < ApplicationRecord
   scope :active, -> { where(deactivated_at: nil) }
   scope :inactive,   -> { where.not(deactivated_at: nil) }
   scope :alphabetized, -> { order('name ASC') }
+
+  # --- Setters & Getters --- #
+  def deactivated; deactivated_at?; end
+  def deactivated= value
+    if ToBoolean(value)
+      self.deactivated_at ||= Time.now
+    else
+      self.deactivated_at = nil
+    end
+  end
 end
