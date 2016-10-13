57fb3f1d66333bb9e6368bb94991005555b61401
Micah <micahyb@gmail.com>
2016-10-12 23:02:16 -0500

    added routes for project controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # resources :clients
   # resources :project_logs
   resources :logs, only: [:create, :edit, :update, :show]
-  # resources :projects
+  resources :projects, only: [:index, :new, :create, :edit, :update]
   resources :users, only: [:new, :create]
   root 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
