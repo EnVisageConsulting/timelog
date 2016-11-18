a86c2f208e8ee01bdb2bc78c94f1a2f05c642a0b
Micah <micahyb@gmail.com>
2016-11-17 23:26:59 -0600

    make flash message appropriate to deactivation action

--- app/controllers/deactivates_controller.rb
+++ app/controllers/deactivates_controller.rb
@@ -7,9 +7,9 @@ class DeactivatesController < ApplicationController
 
     respond_to do |format|
       if @user.deactivate! deactivating?
-        format.html { redirect_to users_path(deactivated: !deactivating?), notice: "#{@user.name} was deactivated successfully!" }
+        format.html { redirect_to users_path(deactivated: !deactivating?), notice: "#{@user.name} was #{'de' if deactivating?}activated successfully!" }
       else
-        format.html { redirect_to users_path(deactivated: !deactivating?), alert: "Unable to deactivate #{@user.name}." }
+        format.html { redirect_to users_path(deactivated: !deactivating?), alert: "Unable to #{'de' if deactivating?}activate #{@user.name}." }
       end
     end
   end
