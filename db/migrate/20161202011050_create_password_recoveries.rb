a0dda5edd038fbf1c01e9f939642ca755a2bcc43
Micah <micahyb@gmail.com>
2016-12-01 19:19:03 -0600

    created table for password reset recovery

--- db/migrate/20161202011050_create_password_recoveries.rb
+++ db/migrate/20161202011050_create_password_recoveries.rb
@@ -1,8 +1,12 @@
 class CreatePasswordRecoveries < ActiveRecord::Migration[5.0]
   def change
     create_table :password_recoveries do |t|
-
-      t.timestamps
+      t.belongs_to :user,     null: false
+      t.string :token,        null: false
+      t.datetime :expired_at, null: false
+      t.timestamps            null: false
     end
+
+    add_index :password_recoveries, :token, unique: true
   end
 end
