2b02bcf5cc86f056381f2f2bbf6b361bc4a77c93
Micah <micahyb@gmail.com>
2016-12-01 19:53:26 -0600

    added password recovery form and creation

--- app/controllers/password_recoveries_controller.rb
+++ app/controllers/password_recoveries_controller.rb
@@ -1,8 +1,17 @@
 class PasswordRecoveriesController < ApplicationController
+  load_resource find_by: :token, param_method: :password_recovery_create_params
+
   def new
   end
 
   def create
+    respond_to do |format|
+      if @password_recovery.save
+        format.html { redirect_to login_path, notice: "Success! Check your email inbox for a password reset link." }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
@@ -10,4 +19,10 @@ class PasswordRecoveriesController < ApplicationController
 
   def update
   end
+
+  private
+
+    def password_recovery_create_params
+      params.require(:password_recovery).permit(:email)
+    end
 end
