461ab3a1f1832d5b435a3398d1b3a075618546f6
Micah <micahyb@gmail.com>
2016-12-01 20:08:04 -0600

    added form and controller action for updating user password

--- app/controllers/password_recoveries_controller.rb
+++ app/controllers/password_recoveries_controller.rb
@@ -1,5 +1,5 @@
 class PasswordRecoveriesController < ApplicationController
-  load_resource find_by: :token, param_method: :password_recovery_create_params
+  load_resource find_by: :token, param_method: :password_recovery_params
 
   def new
   end
@@ -15,14 +15,28 @@ class PasswordRecoveriesController < ApplicationController
   end
 
   def edit
+    @user = @password_recovery.user
   end
 
   def update
+    @user = @password_recovery.user
+
+    respond_to do |format|
+      if @user.update_attributes(user_params)
+        format.html { redirect_to login_path, notice: "Successfully updated password!" }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   private
 
-    def password_recovery_create_params
+    def password_recovery_params
       params.require(:password_recovery).permit(:email)
+    end
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
     end
 end
