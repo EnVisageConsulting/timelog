dd4a863078c4dabe8e66d8822437d98c18b8249a
Micah <micahyb@gmail.com>
2016-12-01 19:36:39 -0600

    added setter for assigning user through email

--- app/models/password_recovery.rb
+++ app/models/password_recovery.rb
@@ -1,3 +1,10 @@
 class PasswordRecovery < ApplicationRecord
+  # --- Associations --- #
   belongs_to :user
+
+  # --- Setters & Getters --- #
+  def email; user.try(:email); end
+  def email=(value)
+    self.user = value.present? ? User.with_email(value) : nil
+  end
 end
