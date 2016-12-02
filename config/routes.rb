c4d7c0bb7f97022734bb337c0a35fbabd4da04e0
Micah <micahyb@gmail.com>
2016-12-01 20:29:26 -0600

    added reports controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :reports, only: :index
+
   get    'login' => 'sessions#new'
   post   'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
