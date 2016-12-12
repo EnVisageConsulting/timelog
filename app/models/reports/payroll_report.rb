69f0b2ed493f8dfe2ccef69f779875cd6dec349c
Micah Brown <micahyb@gmail.com>
2016-12-11 18:24:57 -0600

    added report model for payroll report

---
+++ app/models/reports/payroll_report.rb
@@ -0,0 +1,37 @@
+class Reports::PayrollReport < TablelessModel
+  attr_accessor :user, :start_at, :end_at
+
+  def user_id
+    self.user.try :id
+  end
+
+  def user_id= value
+    self.user = User.find_by(id: value)
+  end
+
+  def start_date
+    return unless start_at
+    raise 'here'.inspect
+  end
+
+  def start_date= value
+    if value.is_a?(String) && value.match(DATE_PATTERN)
+      value = DateTimeParser.string_to_datetime value
+    end
+
+    self.start_at = value
+  end
+
+  def end_date
+    return unless end_at
+    raise 'here'.inspect
+  end
+
+  def end_date= value
+    if value.is_a?(String) && value.match(DATE_PATTERN)
+      value = DateTimeParser.string_to_datetime value
+    end
+
+    self.end_at = value
+  end
+end
