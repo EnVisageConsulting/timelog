f8730c431efc6913eab2f49f63fcd9c34f0656f8
Micah Brown <micahyb@gmail.com>
2016-12-11 18:39:38 -0600

    added form for payroll report

--- app/controllers/reports/payroll_reports_controller.rb
+++ app/controllers/reports/payroll_reports_controller.rb
@@ -1,7 +1,23 @@
 class Reports::PayrollReportsController < ApplicationController
   def new
+    @payroll_report = Reports::PayrollReport.new
   end
 
-  def index
+  def create
+    @payroll_report = Reports::PayrollReport.new(payroll_report_params)
+
+    respond_to do |format|
+      if @payroll_report.valid?
+        format.html { render :report }
+      else
+        format.html { render :new }
+      end
+    end
   end
+
+  private
+
+    def payroll_report_params
+      params.require(:reports_payroll_report).permit(:start_at, :end_at, :user_id)
+    end
 end
