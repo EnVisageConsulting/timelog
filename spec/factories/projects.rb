8315abe89a0ccc9044f90389ebb1f445a293dccf
Micah Brown <micahyb@gmail.com>
2016-12-12 22:25:42 -0600

    added project factories

---
+++ spec/factories/projects.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :project do
+    sequence(:name) { |x| "Project #{x}" }
+  end
+
+  factory :deactivated_project, parent: :project do
+    deactivated_at 1.second.ago
+  end
+
+  factory :invalid_project, parent: :project do
+    name nil
+  end
+end
