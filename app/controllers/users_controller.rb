b7b4e36f2458ff5f9cc8c8c3290e08981615e752
Micah Brown <micahyb@gmail.com>
2016-12-17 12:23:20 -0600

    jump to page using date params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_admin, only: [:index, :edit, :update, :show]
+  before_action :require_admin, only: [:index, :edit, :update]
   load_and_authorize_resource param_method: :user_params
 
   def new
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @logs = @user.logs.active.latest.page(params[:page])
+    @logs = user_logs.page(pagination_page).per(per_page)
   end
 
   private
@@ -43,5 +43,31 @@ class UsersController < ApplicationController
       permitted.push(:role)  if current_user.admin?
 
       params.require(:user).permit(permitted)
+    end
+
+    def user_logs
+      @user_logs ||= @user.logs.active.latest
+    end
+
+    def per_page
+      # return 5 # uncomment for testing with smaller groups
+      Kaminari.config.default_per_page
+    end
+
+    def page_from_date(date)
+      records_from_date = user_logs.where('start_at >= ?', date).count.to_f
+
+      ( records_from_date / per_page ).ceil
+    end
+
+    def pagination_page
+      if params[:date].is_a?(String) && params[:date].match(DATE_PATTERN)
+        date = DateTimeParser.string_to_datetime(params[:date])
+
+        # jump to page using supplied date
+        return page_from_date(date)
+      end
+
+      params[:page]
     end
 end
