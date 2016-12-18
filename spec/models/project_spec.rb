8c7055a3c7c612b33e2b3c64248c404c0e6fc248
Micah Brown <micahyb@gmail.com>
2016-12-18 00:24:45 -0600

    spec for deactivated setter and getter

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { is_expected.to have_many :project_logs }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :name }
+  end
+
+  describe "Scopes" do
+  end
+
+  describe "Setter & Getters" do
+    let(:project) { FactoryGirl.create :project }
+
+    describe "#deactivated" do
+      it "returns truthy value if :deactivated_at is blank" do
+        project.deactivated_at = nil
+
+        expect(project.deactivated).to be_falsey
+      end
+
+      it "return falsey value if :deactivated_at value is set" do
+        project.deactivated_at = 30.minutes.ago
+
+        expect(project.deactivated).to be_truthy
+      end
+    end
+
+    describe "#deactivated=" do
+      context "setting with truthy value" do
+        it "sets :deactivated_at to current time" do
+          expect(project.deactivated_at).to be_nil
+
+          expect {
+            project.deactivated = true
+          }.to change(project, :deactivated_at).to be_within(5.seconds).of Time.now
+        end
+
+        it "doesn't change :deactivated_at if already set" do
+          project.deactivated_at = 30.minutes.ago
+
+          expect {
+            project.deactivated = true
+          }.to_not change(project, :deactivated_at)
+        end
+      end
+
+      context "setting with falsey value" do
+        it "sets :deactivated_at to nil" do
+          project.deactivated_at = 30.minutes.ago
+
+          expect {
+            project.deactivated = false
+          }.to change(project, :deactivated_at).to be_nil
+        end
+      end
+    end
+  end
 end
