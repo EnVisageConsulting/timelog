3efdb9439240183c2fc10ca375f17af751b3fd79
emcollins <ebailey@envisageconsulting.org>
2017-01-11 16:32:37 -0600

    connected filters and continued increasing comprehensive report model and controller

--- app/controllers/reports/comprehensive_reports_controller.rb
+++ app/controllers/reports/comprehensive_reports_controller.rb
@@ -2,4 +2,17 @@ class Reports::ComprehensiveReportsController < ApplicationController
   def index
   	@comprehensive_report = Reports::ComprehensiveReport.new
   end
+
+  def create
+    @comprehensive_report = Reports::ComprehensiveReport.new
+
+    respond_to do |format|
+      if @comprehensive_report.valid?
+        format.html { redirect_to reports_comprehensive_reports_path }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
--- app/models/reports/comprehensive_report.rb
+++ app/models/reports/comprehensive_report.rb
@@ -1,4 +1,53 @@
+require 'date_time_parser'
+
 class Reports::ComprehensiveReport < TablelessModel
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+
+  attr_accessor :project, :user, :start_at, :end_at
+
+  def initialize(attributes = {})
+    set_default_attrs
+    super attributes
+  end
+
+  def start_date
+    return unless start_at.present?
+    DateTimeParser.datetime_to_string start_at, strftime: DATE_STRFTIME
+  end
+
+  def start_date= value
+    if value.is_a?(String) && value.match(DATE_PATTERN)
+      value = DateTimeParser.string_to_datetime(value).beginning_of_day
+    end
+
+    self.start_at = value
+  end
+
+  def end_date
+    return unless end_at.present?
+    DateTimeParser.datetime_to_string end_at, strftime: DATE_STRFTIME
+  end
+
+  def end_date= value
+    if value.is_a?(String) && value.match(DATE_PATTERN)
+      value = DateTimeParser.string_to_datetime(value).end_of_day
+    end
+
+    self.end_at = value
+  end
+
+
+  private
+
+    def set_default_attrs
+      now = Time.now.in_time_zone(TIMEZONE)
+
+      self.end_at   = (now    - 1.week).end_of_week
+      self.start_at = (end_at - 1.week).beginning_of_week
+      self.project  = nil
+      self.user 	= nil
+    end
 
 
 end
