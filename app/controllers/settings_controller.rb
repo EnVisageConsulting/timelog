d509fee73d052a170062f6bcbe54eabab61b7e96
Micah Brown <micahyb@gmail.com>
2017-02-20 23:25:40 -0600

    set email updating to point to settings controller

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,4 +1,20 @@
 class SettingsController < ApplicationController
   def index
   end
+
+  def update
+    respond_to do |format|
+      if current_user.update_attributes(settings_params)
+        format.html { redirect_to settings_path, notice: "Successfully updated settings!" }
+      else
+        format.html { render :index }
+      end
+    end
+  end
+
+  private
+
+    def settings_params
+      params.require(:user).permit(:email)
+    end
 end
