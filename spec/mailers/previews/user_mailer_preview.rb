a0d52c83b6e2b7223acc7ac18a8c880b6ead2888
Dalton Huey <daltonjhuey@gmail.com>
2020-01-14 12:59:57 -0600

    Change preview creds to be more professional

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def new_user_email
-    user = User.new(first: "Michael", last: "Brinkman", email: "fh_pledge@michael.com", password: "bigTimepledge", role: 0)
+    user = User.new(first: "Michael", last: "Brinkman", email: "fh_guy@michael.com", password: "farmHouse", role: 0)
 
     UserMailer.new_user_email(user)
   end
