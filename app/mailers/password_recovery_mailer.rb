d613752725ab52093c1ebdda4edb1b302d5f0a9e
Matt Llewelyn <mjllewelyn@gmail.com>
2020-06-10 10:18:56 -0500

    #34 Tried another method for the logo

--- app/mailers/password_recovery_mailer.rb
+++ app/mailers/password_recovery_mailer.rb
@@ -3,6 +3,7 @@ class PasswordRecoveryMailer < ApplicationMailer
     @password_recovery = password_recovery
     @user = @password_recovery.user
 
+    attachments.inline["EnVisage_Logo.png"] = File.read("#{Rails.root}/app/assets/images/EnVisage_Logo.png")
     mail(to: @user.email, subject: "Reset your password on EnVisage Timelog")
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,14 @@
 class UserMailer < ApplicationMailer
   def new_user_email(new_user)
     @new_user = new_user
+    attachments.inline["EnVisage_Logo.png"] = File.read("#{Rails.root}/app/assets/images/EnVisage_Logo.png")
     mail(to: "csatzler@kansas.net", subject: "New User: #{@new_user.name} on EnVisage Timelog")
   end
 
   def user_activation_email(new_user, password_recovery)
     @new_user = new_user
     @password_recovery = password_recovery
+    attachments.inline["EnVisage_Logo.png"] = File.read("#{Rails.root}/app/assets/images/EnVisage_Logo.png")
     mail(to: @new_user.email, subject: "Activate your account on EnVisage Timelog")
   end
 end
